96191d8372eb98be4a968412d23955b3bae0623a
nobby mulenga <nobbymulenga10@gmail.com>
2025-08-12 14:57:08 +0200

    Add files via upload

---
+++ Nob.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() {
+int p=1;
+do{
+
+   printf("%d\n",  p);
+    p++;
+}
+ while(p<=20);
+
+ return 0;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Nobby.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main() {
+int num;
+printf("Enter a number:\n")
+scanf("%d", &num);
+if(num % 2 == 0){
+printf("%d is even", num
+}
+else{
+printf("%d  is odd",  num);
+}
+
+
+
+return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
